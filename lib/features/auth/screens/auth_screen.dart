bacc2b7c3810b51668a3fdda0faeb36d22adeb24
Sahadev Kunwar <sahadevkunwar@outlook.com>
2023-09-19 16:17:19 +0545

    Basic setup for project and login and signup for are created

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,149 @@
+import 'package:amazon_clone/common/widgets/custom_button.dart';
+import 'package:amazon_clone/common/widgets/custom_textfield.dart';
+import 'package:amazon_clone/constants/global_variable.dart';
+import 'package:flutter/material.dart';
+
+enum Auth { signIn, signUp }
+
+class AuthScreen extends StatefulWidget {
+  static const String routeName = "/auth-screen";
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signUp;
+
+  final _signUpFromKey = GlobalKey<FormState>();
+  final _signInFromKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: GlobalVariables.greyBackgroundCOlor,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+            ),
+            ListTile(
+              tileColor: _auth == Auth.signUp
+                  ? GlobalVariables.backgroundColor
+                  : GlobalVariables.greyBackgroundCOlor,
+              title: const Text(
+                "Create Account",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signUp,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(() {
+                      _auth = val!;
+                    });
+                  }),
+            ),
+            if (_auth == Auth.signUp)
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: GlobalVariables.backgroundColor,
+                child: Form(
+                  key: _signUpFromKey,
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        controller: _emailController,
+                        hintText: 'Email',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        controller: _nameController,
+                        hintText: 'Name',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        controller: _passwordController,
+                        hintText: 'Password',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomButton(
+                        text: "Sign Up",
+                        onTap: () {},
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ListTile(
+              tileColor: _auth == Auth.signIn
+                  ? GlobalVariables.backgroundColor
+                  : GlobalVariables.greyBackgroundCOlor,
+              title: const Text(
+                "Sign In",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signIn,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(() {
+                      _auth = val!;
+                    });
+                  }),
+            ),
+            if (_auth == Auth.signIn)
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: GlobalVariables.backgroundColor,
+                child: Form(
+                  key: _signUpFromKey,
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        controller: _emailController,
+                        hintText: 'Email',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        controller: _passwordController,
+                        hintText: 'Password',
+                      ),
+                      const SizedBox(height: 10),
+                      CustomButton(
+                        text: "Sign In",
+                        onTap: () {},
+                      )
+                    ],
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:amazon_clone/constants/global_variable.dart';
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,10 +16,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon clone',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme:
+            const ColorScheme.light(primary: GlobalVariables.secondaryColor),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
       ),
-      home: const Text('Flutter Demo Home Page'),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
     );
   }
 }
